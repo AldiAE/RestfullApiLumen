0d120e1a08312f1f9a2e33bd2ebc2bd2fbdae17a
AldiAE <aldirosyad68@gmail.com>
2020-12-03 12:22:52 +0700

    Make a bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -92,9 +92,9 @@ $app->middleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AppServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 
 /*
